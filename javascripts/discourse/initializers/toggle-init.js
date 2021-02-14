2fab3439bbe170ff4ecb3bb2f2c52664485b25af
Faizaan Gagan <fzngagan@gmail.com>
2021-02-14 12:30:43 +0530

    hide toggle button for first post

--- javascripts/discourse/initializers/toggle-init.js
+++ javascripts/discourse/initializers/toggle-init.js
@@ -26,6 +26,8 @@ const whisperInit = (api) => {
     });
 
     api.addPostMenuButton("toggleWhisper", (model) => {
+      if (model.post_number < 2) return;
+
       let isWhisper = model.post_type === 4;
       let icon = isWhisper ? "far-eye" : "far-eye-slash";
       let title = isWhisper

694cd0844b75d48670970c6c8fdff5ff50be546b
Lhc_fl <lhcfl@outlook.com>
2025-01-20 23:18:08 +0800

    Use new APIs instead of deprecated ones
    See https://meta.discourse.org/t/upcoming-post-menu-changes-how-to-prepare-themes-and-plugins/341014

--- javascripts/discourse/initializers/toggle-init.js
+++ javascripts/discourse/initializers/toggle-init.js
@@ -1,43 +1,18 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { ajax } from "discourse/lib/ajax";
-import { popupAjaxError } from "discourse/lib/ajax-error";
+import WhisperToggle from "../components/whisper-toggle";
 
 export default {
   name: "toggle-whispers",
   initialize() {
-    withPluginApi("0.11.0", whisperInit);
+    withPluginApi("0.11.0", (api) => {
+      api.registerValueTransformer(
+        "post-menu-buttons",
+        ({ value: dag, context: { lastHiddenButtonKey } }) => {
+          dag.add("post-folding", WhisperToggle, {
+            before: lastHiddenButtonKey,
+          });
+        }
+      );
+    });
   },
-};
-
-const whisperInit = (api) => {
-  const currentUser = api.getCurrentUser();
-  if (currentUser && currentUser.staff && currentUser.whisperer) {
-    api.attachWidgetAction("post-menu", "toggleWhisper", function () {
-      const model = this.attrs;
-      let newType = model.post_type === 1 ? 4 : 1;
-
-      ajax(`/posts/${model.id}/post_type`, {
-        type: "PUT",
-        data: {
-          post_type: newType,
-        },
-      }).catch(popupAjaxError);
-    });
-
-    api.addPostMenuButton("toggleWhisper", (model) => {
-      if (model.post_number < 2) return;
-
-      let isWhisper = model.post_type === 4;
-      let icon = isWhisper ? "far-eye" : "far-eye-slash";
-      let title = isWhisper
-        ? "toggle_button_title.regular"
-        : "toggle_button_title.whisper";
-      return {
-        action: "toggleWhisper",
-        icon: icon,
-        title: themePrefix(title),
-        position: "second-last-hidden",
-      };
-    });
-  }
 };
